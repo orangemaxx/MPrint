07767d72e5c7ea683718b81142781e56f9774dbb
Max H <49844637+orangemaxx@users.noreply.github.com>
2025-02-27 07:40:42 +1300

    Started Login page / system and reworked file for easy login

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session, url_for
+from login import LoginAuth
 
 app = Flask(__name__)
 
+app.secret_key = ["tjehjeflkjdjfkljIOjoifjdijfkfjKLJDKSJFUIOefhuei"]
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if session.get("logged_in"):
+        return redirect(url_for('dash'))
+    else:
+        session.clear()
+        return redirect(url_for('login'))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        # Base login Page
+        return render_template("index.html")
+    else:
+        pass
+
+@app.route("/dashboard", methods=["GET"])
+def dash():
+    return("Hello")
 
 app.run(debug=True)
---
+++ login.py
@@ -0,0 +1,3 @@
+def LoginAuth(user, passw):
+    print(user + " " + passw)
+    return zip(1, 1)
