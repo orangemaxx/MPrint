448f64d5c6245c6b3057eb28b8b95b1acd2f1646
Max H <49844637+orangemaxx@users.noreply.github.com>
2025-02-28 10:44:07 +1300

    Terrible login system fix later

--- sqldb.py
+++ sqldb.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 from os import getenv
 load_dotenv()
 import settings
+from sqlalchemy import text
+from string import ascii_letters
+from random import choice
 
-
-
+loginIdLen = 30
 
 def connectDB():
     Database = mysql.connector.connect(
@@ -19,10 +21,32 @@ def connectDB():
     return Database
 
 def LoginUser(user, passw, session):
-    # Database = settings.Database
-    # query = "SELECT * FROM users WHERE username = :user"
-    # mycursor = Database.cursor()
-    # mycursor.execute(query,{"user":user}).fetchone()
-    # print(mycursor)
     Database = settings.Database
-    query = text("SELECT * FROM userdata ")
+    query = "SELECT password FROM userdata WHERE username = %s"
+    
+    # Use parameterized queries to avoid SQL injection risks
+    cursor = Database.cursor()
+    cursor.execute(query, (user,))
+    result = cursor.fetchone()[0] # Fetch the result
+    print(result)
+    if passw == result:
+        
+        query = "INSERT INTO userdata (sessionid) VALUES (%s) WHERE username = %s"
+        cursor.execute(query, (LoginIdGen(Database), user))
+        Database.commit()
+
+def LoginIdGen(Database):
+    loginId = ""
+    while True:
+        for i in range(loginIdLen):
+            loginId += choice(ascii_letters)
+        query = "SELECT sessionid FROM userdata"
+        for x in Database.cursor().execute(query).fetchall():
+            if x == loginId:
+                exists = True
+                break
+            else: 
+                exists = False
+        if not exists:
+            break     
+    return loginId
