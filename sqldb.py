c0c2ed08d702f3161864a3bcfa87c24bc35e0d64
Max H <49844637+orangemaxx@users.noreply.github.com>
2025-02-28 10:17:49 +1300

    More database stuff special thanks clyde

--- sqldb.py
+++ sqldb.py
@@ -6,22 +6,23 @@ import settings
 
 
 
+
 def connectDB():
     Database = mysql.connector.connect(
-    host=getenv("DATABASE"),
+    host=getenv("DATABASE_HOST"),
     port=getenv("DATABASE_PORT"),
     user=getenv("DATABASE_USERNAME"),
     password=getenv("DATABASE_PASSWORD"),
-    database=getenv("DATABASE_SCHEMA")
+    database=getenv("DATABASE")
     )
 
     return Database
 
 def LoginUser(user, passw, session):
+    # Database = settings.Database
+    # query = "SELECT * FROM users WHERE username = :user"
+    # mycursor = Database.cursor()
+    # mycursor.execute(query,{"user":user}).fetchone()
+    # print(mycursor)
     Database = settings.Database
-    query = "SELECT * FROM users WHERE username =" + user
-    mycursor = Database.cursor()
-    mycursor.execute(query)
-    result = mycursor.fetchall()
-    for x in result:
-        print(x)
+    query = text("SELECT * FROM userdata ")
