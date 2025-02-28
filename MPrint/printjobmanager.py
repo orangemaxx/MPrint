b053ab4f64f902cb32399cfa5fb7f5f1ab509a77
Max H <49844637+orangemaxx@users.noreply.github.com>
2025-03-01 09:59:56 +1300

    Commented Printjobmanager.py better for fixing later on

--- MPrint/printjobmanager.py
+++ MPrint/printjobmanager.py
@@ -17,19 +17,29 @@ Database = settings.Database
 def createPrintJob(filename, color, pages, copies):
     # FIXME: AttributeError: 'NoneType' object has no attribute 'cursor'
     # https://paste.mozilla.org/60K3T2kj
+
+    # TODO: ALMOST FORGOT TO ADD JOB EXPIRRY DO THAT SOON!!! Gotta be able to delete the files because we are not doing allat.
+    # TODO: ALSO PLEASE NOTE SO FAR TS DOES NOT SAVE THE FILE. ONLY DATABASE STUFF SO FAR. UNDECIDED IF I WANT TO SAVE THE FILE HERE OR SOMEWHERE ELSE. MIGHT BE EASIER TO HAVE IT ALL CENTRALISED
+
+    # TODO: Hoping clyde can debug this
     Database = settings.Database
     if not checkLogin(Database):
         return False
-    print(Database)
-    mycursor = Database.cursor()
+    print(Database) # TODO: Testing purposes. Get rid of this later
+    # Set the cursor for query
+    mycursor = Database.cursor() # <- this is where the error keeps appearing. Please note there is error prevention in place. If we can't connect to the database the app will not run.
+    # Create the job id
     jobId = createJobId()
+    # Get the userid
     userid = session.get("userId")
+    # Set the query for mysql
     query = "INSERT INTO printjobs (jobid, userid, jobfilename, jobscolour, pages, copies) VALUES (%s, %s, %s, %s, %s, %s)"
+    # Set the variables
     val = (jobId, userid, filename, color, pages, copies)
-    print("yeah")
-    mycursor.execute(query, val)
-    Database.commit()
-    return True
+    print("yeah") # TODO: This is to check it got this far. Get rid of this later
+    mycursor.execute(query, val) # Execute command in mysql database
+    Database.commit() # commit to database
+    return True # return true if succesful
 
 
 def createJobId():
